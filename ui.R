947f1bc09f7d65c6e06aee159f25775c6e3620a2
Mazen Jabbour <73285095+mazenjabbour@users.noreply.github.com>
2020-12-03 00:23:06 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,21 +1,16 @@
 library(shiny)
-shinyUI(fluidPage(
-  titlePanel("Scatter Plot"),
-  sidebarLayout(
+
+shinyUI( pageWithSidebar(
+    headerPanel("Rectangle Area Calculation"),
+    
     sidebarPanel(
-      numericInput("numeric", "Number of points", 
-                   value = 5000, min = 1, max = 5000, step = 1),
-      sliderInput("sliderX", "Select X range",
-                  -500, 500, value = c(-50, 50)),
-      sliderInput("sliderY", "select y range",
-                  -500, 500, value = c(-50, 50)),
-      checkboxInput("show_xlab", "Show/Hide X Axis Label", value = TRUE),
-      checkboxInput("show_ylab", "Show/Hide Y Axis Label", value = TRUE),
-      checkboxInput("show_title", "Show/Hide Title")
-    ),
+        textInput (inputId = 'length', 'Enter Length', value = '0'),
+        textInput (inputId = 'breadth', 'Enter Width', value = '0')
+    ), 
     mainPanel(
-      h3("Enjoy the graph"),
-      plotOutput("plot1")
+        h4('The Area of your rectangle is'),
+        verbatimTextOutput("area")
     )
-  )
-))
+)
+)
+
